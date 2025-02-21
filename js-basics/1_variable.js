f41b5ade03b13b8fa2bd944f51969dad34dfea36
Divyansh Agrawal <128778644+MrDivyanshAgrawal@users.noreply.github.com>
2025-02-21 05:59:48 +0000

    basics done

---
+++ js-basics/1_variable.js
@@ -0,0 +1,11 @@
+const accountId=12334
+let accountEmail="demo@gmail.com"
+var accountPassword="12389"
+accountCity="Kota"
+let accountState
+// accountId=3
+accountEmail="test@gmail.com"
+accountPassword="34567"
+accountCity="Indore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
