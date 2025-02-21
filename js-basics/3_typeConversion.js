f08b4dbec76d30fd8464b03e40e0521a756ca2b4
Divyansh Agrawal <128778644+MrDivyanshAgrawal@users.noreply.github.com>
2025-02-21 07:22:35 +0000

    basics done

---
+++ js-basics/3_typeConversion.js
@@ -0,0 +1,17 @@
+const score="Divyansh"
+
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNum=Number(score)
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+let isLoggedIn=1
+let BooleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn);
+
+let someNum=33
+let stringNum=String(someNum)
+console.log(stringNum);
+console.log(typeof(stringNum));
+
