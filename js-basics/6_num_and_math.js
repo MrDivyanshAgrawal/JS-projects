0735d4b8212ac4d0f6b3e4e53d6ec3caa1ec8321
Divyansh Agrawal <128778644+MrDivyanshAgrawal@users.noreply.github.com>
2025-06-05 14:30:16 +0000

    done

---
+++ js-basics/6_num_and_math.js
@@ -0,0 +1,40 @@
+const score=100
+// console.log(score);
+
+const newScore=new Number(200)
+// console.log(newScore);
+
+// console.log(newScore.toString().length);
+// console.log(newScore.toFixed(2));
+
+const otherNum=123.4564
+// console.log(otherNum.toPrecision(4));
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-In'));
+
+// Maths
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(3.5));
+// console.log(Math.ceil(3.4));
+// console.log(Math.floor(3.9));
+// console.log(Math.min(2,4,3,5));
+// console.log(Math.max(2,4,3,5));
+// console.log(Math.pow(2,4));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
