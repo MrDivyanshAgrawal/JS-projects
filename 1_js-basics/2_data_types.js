e6aab891c13294795b3bcf8e8a25edcfd826c39d
Divyansh Agrawal <128778644+MrDivyanshAgrawal@users.noreply.github.com>
2025-06-06 13:12:03 +0000

    done

---
+++ 1_js-basics/2_data_types.js
@@ -0,0 +1,12 @@
+"use strict"
+// alert(3+3)
+
+let name="Divyansh"
+let age=21
+let isLoggedIn=false
+console.log(typeof("Divyansh"));
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
