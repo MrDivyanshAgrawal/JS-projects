5d7b34bc5a5df6e73a81e91bf842393193df9f3e
Divyansh Agrawal <128778644+MrDivyanshAgrawal@users.noreply.github.com>
2025-06-10 14:28:17 +0000

    done

---
+++ 4-control-loop/switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+//     default:
+//         break;
+// }
+
+const month = "Jan"
+switch(month){
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "Mar":
+        console.log("March");
+        break;
+    case "Apr":
+        console.log("April");
+        break;
+    default:
+        console.log("default case match");
+        break;   
+}
